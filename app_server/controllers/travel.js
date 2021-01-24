a5ff5509584a26454f5dc8532e8fd2593246d719
Steven <look_steven@yahoo.com>
2021-01-24 15:29:24 -0500

    Express website, static HTML only

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,8 @@
-/* GET travel view */
-const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
-   };
-   module.exports = {
-    travel
-   };
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/travel');
+
+/* GET home page. */
+router.get('/', controller.travel);
+
+module.exports = router;
